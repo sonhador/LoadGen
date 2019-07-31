be56219d302f3772164c52894f039d17b8a2140b
MongJu Jung <mjjung@gmail.com>
2019-07-31 22:09:58 +0900

    minor change.

--- run_filegen.sh
+++ run_filegen.sh
@@ -1,1 +1,1 @@
-hadoop jar ./target/loadgen-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dell.korea.psd.bigdata.mr.filegen.FileGen hdfs://<HDFS_HOSTNAME_OR_IP>/tmp/<PATH> 1048576 100 100
+hadoop jar ./dist/loadgen-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dell.korea.psd.bigdata.mr.filegen.FileGen hdfs://<HDFS_HOSTNAME_OR_IP>/tmp/<PATH> 1048576 100 100
--- run_loadgen.sh
+++ run_loadgen.sh
@@ -1,1 +1,1 @@
-hadoop jar ./target/loadgen-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dell.korea.psd.bigdata.mr.loadgen.LoadGen hdfs://<HDFS_HOSTNAME_OR_IP>/tmp/<PATH> 83886080 10 180
+hadoop jar ./dist/loadgen-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dell.korea.psd.bigdata.mr.loadgen.LoadGen hdfs://<HDFS_HOSTNAME_OR_IP>/tmp/<PATH> 83886080 10 180
