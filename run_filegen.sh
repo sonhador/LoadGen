be7047cecebe4185845ca253abbfd181f9fa1d53
MongJu Jung <mjjung@gmail.com>
2019-07-31 22:12:03 +0900

    added comment to run-scripts.

--- run_filegen.sh
+++ run_filegen.sh
@@ -1,1 +1,3 @@
+# args: <output_hdfs_path> <bytes_per_file_to_generate> <number_of_files_to_generate_per_mapper> <number_of_mappers>
+
 hadoop jar ./dist/loadgen-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dell.korea.psd.bigdata.mr.filegen.FileGen hdfs://<HDFS_HOSTNAME_OR_IP>/tmp/<PATH> 1048576 100 100
--- run_loadgen.sh
+++ run_loadgen.sh
@@ -1,1 +1,3 @@
+# args: <output_hdfs_path> <bytes_per_line_to_generate> <load_gen_period_in_minutes> <number_of_mappers>
+
 hadoop jar ./dist/loadgen-0.0.1-SNAPSHOT-jar-with-dependencies.jar com.dell.korea.psd.bigdata.mr.loadgen.LoadGen hdfs://<HDFS_HOSTNAME_OR_IP>/tmp/<PATH> 83886080 10 180
